b37521c69060a266dc3fe34056088ca22e4b9646
majharul97 <majharuli97@gmail.com>
2019-10-19 12:59:14 +0100

    Update script.js

--- work/js/script.js
+++ work/js/script.js
@@ -1,6 +1,6 @@
-var i = 0;
-do {
-  i++
+var y = new Boolean(false);
+
+if (y = false) {
   $('.social').hide(function() {
   $(this).delay(12000).fadeIn(800);
 });
@@ -9,4 +9,3 @@ $('.rw-sentence')
   .delay(10000)
   .fadeOut(400);
 }
-while (i <= 10);
